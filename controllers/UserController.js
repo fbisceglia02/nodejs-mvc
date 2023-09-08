2ef4ee0ae01229395d045a646aada5e1275914b4
Ferruccio Islam Bisceglia <120172848+fbisceglia02@users.noreply.github.com>
2023-09-08 10:10:59 +0000

    feat/add user controller

---
+++ controllers/UserController.js
@@ -0,0 +1,8 @@
+/* this object will contain all the http requests that we will make 
+with regards to the user */ 
+module.exports = {
+
+    get : (req, res) => {
+        res.send("User: fbisceglia02")
+    },
+}
--- routes/User.js
+++ routes/User.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router()
-
+const userController = require('../controllers/UserController')
 // post request to a User
-router.get('/', (req, res) => {
-    res.send("User: fbisceglia02")
-});
+router.get('/', userController.get);
 
 module.exports = router;
